961fd768c0fd0929ce2fcf74192b39ab71b2a9cf
jrandiny <jrandiny2@gmail.com>
2019-03-13 19:49:58 +0700

    Fix name validator

--- Server/app.js
+++ Server/app.js
@@ -24,18 +24,6 @@ app.get('/events',(req,res)=>{
             'status':500
         });
     });
-
-    /*db.collection(dbPath.events).get().then((snapshot)=>{
-        let items = snapshot.docs.map(item => {
-            return {'id':item.id, ...item.data()};
-        });
-        res.json(items);
-    }).catch(err => {
-        res.status(500).json({
-            'detail':err,
-            'status':500
-        });
-    });*/
 });
 
 app.get('/events/:id',(req,res)=>{
@@ -54,30 +42,15 @@ app.get('/events/:id',(req,res)=>{
             'status':500
         });
     });
-
-    /*db.collection(dbPath.events).doc(req.params.id).get().then((snapshot)=>{
-        if(!snapshot.exists){
-            res.status(404).json({
-                'detail':'ID not found',
-                'status':404
-            });
-        }else{
-            res.json(snapshot.data());
-        }
-    }).catch(err => {
-        res.status(500).json({
-            'detail':err,
-            'status':500
-        });
-    });*/
 });
 
 app.post('/events',[
-        /*check('name')
+        check('name')
         .trim()
         .escape()
+        .blacklist(' ')
         .isAlphanumeric()
-        .exists()*/
+        .exists()
     ],(req,res)=>{
         var errors = validationResult(req);
 
@@ -102,61 +75,8 @@ app.post('/events',[
                     'status':500
                 });
             });
-
-            /*db.collection(dbPath.events).add({
-                name: req.body.name,
-                logo: req.body.logo
-            }).then((ref)=>{
-                console.log('yay');
-                res.location('/events/'+ref.id).sendStatus(200);
-            }).catch((err)=>{
-                res.status(500).json({
-                    'detail':err,
-                    'status':500
-                });
-            })*/
         }
 });
-
-/*app.post('/events',[
-        check('name')
-        .trim()
-        .escape()
-        .isAlphanumeric()
-        .exists(),
-        check('logo')
-        .trim()
-        .isURL()
-        .exists()
-    ],(req,res)=>{
-        var errors = validationResult(req);
-
-        if (!errors.isEmpty()) {
-            let field = new Set();
-
-            errors.array().forEach((item)=>{
-                field.add(item.param);                
-            });
-
-            res.status(400).json({
-                'detail':'Invalid value for '+[...field].join(', '),
-                'status':400
-            });
-        } else {
-            db.collection(dbPath.events).add({
-                name: req.body.name,
-                logo: req.body.logo
-            }).then((ref)=>{
-                res.location('/events/'+ref.id).sendStatus(200);
-            }).catch((err)=>{
-                res.status(500).json({
-                    'detail':err,
-                    'status':500
-                });
-            })
-            
-        }
-});*/
 
 app.listen(port, () => {
     console.log(`Server started on ${port}!`);
