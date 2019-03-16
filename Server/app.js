e77f3dee4401559c3fb325bee9db78945d336b4f
jrandiny <jrandiny2@gmail.com>
2019-03-17 00:03:25 +0700

    Add cors preflight

--- Server/app.js
+++ Server/app.js
@@ -1,17 +1,14 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors')
 
 const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Headers");
-    res.header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS, PUT");
-    next();
-});
+app.use(cors());
 
+app.options('*', cors());
 app.use('/students', require('./auth'), require('./routes/students'));
 app.use('/events', require('./auth'), require('./routes/events'));
 app.use('/users', require('./auth'), require('./routes/users'));
