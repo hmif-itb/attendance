63fde82355540bb3110ac59e4822f58087d8ecea
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2019-03-16 23:20:45 +0700

    Added CORS headers

--- Server/app.js
+++ Server/app.js
@@ -7,7 +7,8 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Headers");
+    res.header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS, PUT");
     next();
 });
 
